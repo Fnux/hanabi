ed40c7256cbd9b57590c249f521ed5ece3ed3394
Timothée Floure <timothee.floure@fnux.ch>
2017-09-07 08:55:48 +0200

    Refactor channels' tests

--- test/hanabi/channel_test.exs
+++ test/hanabi/channel_test.exs
@@ -3,60 +3,74 @@ defmodule HanabiTest.Channel do
   use ExUnit.Case
   use Hanabi.IRC.Numeric
 
+  @alpha HanabiTest.build_test_user("alpha")
+  @beta HanabiTest.build_test_user("beta")
+  @gamma HanabiTest.build_test_user("gamma")
+
+  @greek HanabiTest.build_test_channel("#greek")
+  @latin HanabiTest.build_test_channel("#latin")
+
   setup do
+    # Remove existing data
     User.flush_registry
     Channel.flush_registry
 
-    # add users
-    for nick <- ["lambda", "beta", "gamma"] do
-      {key, user} = HanabiTest.build_test_user(nick)
-      User.set key, user
+    # Add test users and channels
+    for user <- [@alpha, @beta, @gamma] do
+      User.set user.key, user
+    end
+    for channel <- [@greek, @latin] do
+      Channel.set channel.name, channel
     end
 
-    # Add channels
-    {key, channel} = HanabiTest.build_test_channel("#greek")
-    Channel.set key, channel
-
-    #
-    Channel.add_user(:beta, "#greek")
-    Channel.add_user(:gamma, "#greek")
+    Channel.add_user(@beta, @greek)
+    Channel.add_user(@beta, @latin)
+    Channel.add_user(@gamma, @latin)
 
     :ok
   end
 
   test "Add an user to a channel" do
-
+    # Add an user to a non-existing channel
     assert Channel.get("#hanabi") == nil
-    {status, updated_channel} = Channel.add_user(:lambda, "#hanabi")
+    {status, updated_channel} = Channel.add_user(@alpha.key, "#hanabi")
     assert status == :ok
     assert Channel.get("#hanabi") == updated_channel
-    assert :lambda in updated_channel.users
+    assert updated_channel.users == [@alpha.key]
 
+    # Add a non-existent user
     assert Channel.add_user("unknown", "#hanabi") == {:err, :no_such_user}
-    assert Channel.get("#hanabi").users == [:lambda]
+    assert Channel.get("#hanabi").users == [@alpha.key]
+
+    assert @beta.key in Channel.get(@latin.name).users
+    assert @gamma.key in Channel.get(@latin.name).users
 
     # Check JOIN messages @TODO
   end
 
   test "Remove an user from a channel" do
-    assert Channel.remove_user(:beta, "#unknown") == {:err, @err_nosuchchannel}
-    assert Channel.remove_user(:gamma, "#hanabi") == {:err, @err_notonchannel}
-    assert Channel.remove_user(:unknown, "#greek") == {:err, :no_such_user}
+    assert Channel.remove_user(@alpha, "#unknown") == {:err, @err_nosuchchannel}
+    assert Channel.remove_user(@alpha, @greek) == {:err, @err_notonchannel}
+    assert Channel.remove_user(@alpha.key, @greek.name) == {:err, @err_notonchannel}
+    assert Channel.remove_user(:unknown, @greek) == {:err, :no_such_user}
 
-    assert :beta in Channel.get("#greek").users
-    {status, updated_channel} = Channel.remove_user(:beta, "#greek")
+    assert @beta.key in Channel.get(@greek.name).users
+    {status, updated_channel} = Channel.remove_user(@beta.key, @greek.name)
     assert status == :ok
-    assert :beta not in updated_channel.users
+    assert @beta.key not in updated_channel.users
 
     # Check PART messages @TODO
   end
 
   test "Set the topic of a channel" do
-    assert Channel.set_topic("#greek", "New topic!") == :ok
-    assert Channel.get("#greek").topic == "New topic!"
+    assert Channel.get(@greek.name).topic != "New topic!"
+    assert Channel.set_topic(@greek, "New topic!") == :ok
+    assert Channel.get(@greek.name).topic == "New topic!"
+    assert Channel.set_topic(@greek.name, "Wrong topic!") == :ok
+    assert Channel.get(@greek.name).topic != "New topic!"
 
     assert Channel.set_topic("#unknown", "New topic!") == :err
-    assert Channel.set_topic("#greek", nil) == :err
+    assert Channel.set_topic(@greek, nil) == :err
 
     # Check TOPIC IRC messages @TODO
   end
@@ -64,13 +78,13 @@ defmodule HanabiTest.Channel do
   test "Send a message to a channel" do
     msg = %Message{command: "TEST"}
 
-    Channel.broadcast("#greek", msg)
+    Channel.broadcast(@greek, msg)
 
     # Check received messages @TODO
   end
 
   test "Get the nicknames of a channel's users" do
-    list = Channel.get("#greek").users |> Channel.get_names
+    list = Channel.get(@latin.name).users |> Channel.get_names
 
     assert list == "beta gamma"
   end
